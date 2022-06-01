3a2d961ec7d316be7b0c752c78afb78de9fa8608
Danil Loginov <loginovds@yandex.ru>
2022-06-01 15:48:25 +0300

    C++

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <sdt.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russia");
+    cout << "введите x: ";
+    int x, y;
+    cin >> x;
+    cout << "введите y: ";
+    cin >> y;
+    cout << " x " << x << endl;
+    cout << " y " << y << endl;
+    return 0;
+}
+
